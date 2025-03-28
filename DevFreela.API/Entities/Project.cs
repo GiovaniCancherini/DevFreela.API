cb40e527aab1d2c603b9e44328184bdf6fc61aa5
Giovani Cancherini <giovani.canquerini@gmail.com>
2025-03-28 11:20:19 -0300

    feat: Entity

---
+++ DevFreela.API/Entities/Project.cs
@@ -0,0 +1,72 @@
+﻿using DevFreela.API.Controllers;
+using DevFreela.API.Enums;
+
+namespace DevFreela.API.Entities
+{
+    public class Project : BaseEntity
+    {
+        protected Project() { }
+
+        public Project(string tittle, string decription, int idClient, int idFreelancer, decimal totalCost)
+            : base()
+        {
+            Tittle = tittle;
+            Decription = decription;
+            IdClient = idClient;
+            IdFreelancer = idFreelancer;
+            TotalCost = totalCost;
+
+            Status = ProjectControllerEnum.Created;
+            Comments = new List<ProjectComment>();
+        }
+
+        public string Tittle { get; private set; }
+        public string Decription { get; private set; }
+        public int IdClient { get; private set; }
+        public User Client { get; private set; }
+        public int IdFreelancer { get; private set; }
+        public User Freelancer { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+        public ProjectControllerEnum Status { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
+
+        public void Start()
+        {
+            if (Status == ProjectControllerEnum.Created)
+            {
+                Status = ProjectControllerEnum.InProgress;
+                StartedAt = DateTime.Now;
+            }
+        }
+
+        public void Cancel()
+        {
+            if (Status == ProjectControllerEnum.InProgress)
+            {
+                Status = ProjectControllerEnum.Cancelled;
+                FinishedAt = DateTime.Now;
+            }
+        }
+
+        public void Complete()
+        {
+            if (Status == ProjectControllerEnum.PaymentPending ||
+                Status == ProjectControllerEnum.InProgress)
+            {
+                Status = ProjectControllerEnum.Completed;
+                CompletedAt = DateTime.Now;
+            }
+        }
+
+        public void SetPaymentPending()
+        {
+            if (Status == ProjectControllerEnum.InProgress)
+            {
+                Status = ProjectControllerEnum.PaymentPending;
+            }
+        }
+    }
+}
---
+++ DevFreela.API/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class User : BaseEntity
+    {
+        protected User() { }
+
+        public User(string fullname, string email, DateTime birthDate)
+            : base()
+        {
+            Fullname = fullname;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+            Comments = new List<ProjectComment>();
+        }
+
+        public string Fullname { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public bool Active { get; private set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProjects { get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
+    }
+}
