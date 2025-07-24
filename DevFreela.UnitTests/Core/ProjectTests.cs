d6f8207750655fed3974679c387648d7ca16b7cb
Giovani Cancherini <giovani.canquerini@gmail.com>
2025-07-23 21:03:46 -0300

    feat: add first XUnit tests
    - Introduced a new test class `ProjectTests` to validate the `Start` method's behavior, including success and exception scenarios. Created `DevFreela.UnitTests.csproj` for unit testing setup and updated the solution file to include the new tests project.
    
    - Refactor Project status handling
    Updated the `Project` class to use `ProjectStatusEnum` instead of `ProjectControllerEnum`, modifying relevant methods and adding an `INVALID_STATE_MESSAGE` for exception handling.

---
+++ DevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,52 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core
+{
+    public class ProjectTests
+    {
+        #region Start
+
+        #region Sucess
+        [Fact]
+        public void ProjectIsCreated_Start_Sucess()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descricao Projeto", 1, 2, 1000.1m);
+
+            // Act
+            project.Start();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+            Assert.True(project.Status == ProjectStatusEnum.InProgress);
+            Assert.False(project.StartedAt is null);
+        }
+        #endregion
+
+        #region Failure
+
+        #endregion
+
+        #region Exception
+        [Fact]
+        public void ProjectIsInvalidState_Start_ThrowsException()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descricao Projeto", 1, 2, 1000.1m);
+            project.Start();
+
+            // Act
+            Action? start = project.Start;
+
+            // Assert
+            var exception = Assert.Throws<InvalidOperationException>(start);
+            Assert.Equal(Project.INVALID_STATE_MESSAGE, exception.Message);
+        }
+        #endregion
+
+        #endregion
+    }
+}
